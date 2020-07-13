edb7c2d3cb6d30dfea4163a60350bf8f471d6113
Pedro <pedro.lima@ccc.ufcg.edu.br>
2020-07-13 13:33:08 -0300

    added new input option in the overview card

--- rodolfo/components/Repository/index.js
+++ rodolfo/components/Repository/index.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+
+import getRealm from '../../services/realm';
 import {
   Container,
   Name,
@@ -12,10 +14,51 @@ import {
   NameTrue,
   GradeAverage,
   Notes,
+  Form,
+  Input,
+  Submit,
 } from './styles';
+import Plus from 'react-native-vector-icons/MaterialIcons';
 import Icon from 'react-native-vector-icons/FontAwesome';
 const Repository = ({data}) => {
   const [render, setRender] = useState(false);
+  const [stringNotes, setString] = useState('');
+  const [grade, setGrade] = useState(0);
+  const [error, setError] = useState(false);
+  const name = data.materia;
+
+  async function handleAddRepository() {
+    try {
+      saveRepository();
+      console.log('deu certo');
+      alert('Matéria cadastrada com sucesso');
+      setError(false);
+      console.log(grade);
+      console.log(stringNotes);
+    } catch (err) {
+      setError(true);
+      console.log('não cadastrou');
+      alert('Erro, tente novamente');
+    }
+  }
+
+  async function saveRepository() {
+    const realm = await getRealm();
+
+    realm.write(() => {
+      realm.create(
+        'Repository',
+        {
+          materia: name,
+          goal: data.goal,
+          notes: [stringNotes],
+          grades: [parseFloat(grade, 10)],
+        },
+        true,
+      );
+    });
+    return data;
+  }
   function setRenderingTrue() {
     setRender(true);
   }
@@ -48,6 +91,34 @@ const Repository = ({data}) => {
           <Name>Suas Anotações:</Name>
           <Notes>{data.notes}</Notes>
         </StatsTrue>
+
+        <Form>
+          <Input
+            value={stringNotes}
+            error={error}
+            onChangeText={setString}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Anotações"
+            keyboardType="default"
+          />
+        </Form>
+
+        <Form>
+          <Input
+            value={grade}
+            error={error}
+            onChangeText={setGrade}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Nova nota"
+            keyboardType="default"
+          />
+        </Form>
+
+        <Submit onPress={handleAddRepository}>
+          <Plus name="add" size={42} color="#FFF" />
+        </Submit>
 
         <Refresh onPress={setRederingFalse}>
           <Icon name="arrow-left" color="#7159c1" size={16} />
